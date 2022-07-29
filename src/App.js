ea742b5106ad5d14546ac619a113e0da4a28762c
pritam <pritam.debnath@grorapid.com>
2022-07-29 16:03:34 +0530

    added bot gif and joke fetching feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css'
+import React,{useState,useEffect} from 'react'
+
+let App =()=>{
+  
+  let [joke,setJoke] = useState({})
+  
+  let [jokeExist,setJokeExist] = useState(false)
+  
+  let [show,setShow]=useState('')
+  
+  useEffect(()=>{
+    getJoke()
+  },[])
+  
+ let getJoke = async ()=>{
+   try {
+     setShow('')
+       let apiUrl = 'https://v2.jokeapi.dev/joke/Any?type=twopart' ;
+
+     let res = await fetch(apiUrl)
+     
+     let data = await res.json()
+     
+     setJoke(data)
+     setJokeExist(!jokeExist)
+     
+console.log(data)
+} catch (e) {
+     console.log(e)
+   }
+ }
+ 
+ let showAns = ()=>{
+   setShow('show')
+   setJokeExist(!jokeExist)
+ }
+  
+  return(
+    <div className='bot_container'>
+ <div className='joke'>
+     <h2>{joke.setup}
+     </h2>    
+     <h3 className={show}> { joke.delivery }
+     </h3>
+     
+     </div>
+     
+     <button onClick={jokeExist ? showAns: getJoke}>{jokeExist ? 'show' : 'New joke'}
+     </button>
     </div>
-  );
+    )
 }
-
-export default App;
+export default App
