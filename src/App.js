d25be8ed2a76ff445c88352a79397505cd577b99
pritam <pritam.debnath@grorapid.com>
2022-07-30 20:54:04 +0530

    version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,55 +1,90 @@
+import "./App.css";
 
-import './App.css'
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
+let ssu = new SpeechSynthesisUtterance();
 
-let App =()=>{
-  
-  let [joke,setJoke] = useState({})
-  
-  let [jokeExist,setJokeExist] = useState(false)
-  
-  let [show,setShow]=useState('')
-  
-  useEffect(()=>{
-    getJoke()
-  },[])
-  
- let getJoke = async ()=>{
-   try {
-     setShow('')
-       let apiUrl = 'https://v2.jokeapi.dev/joke/Any?type=twopart' ;
+let App = () => {
+  let ssg = speechSynthesis.getVoices();
 
-     let res = await fetch(apiUrl)
-     
-     let data = await res.json()
-     
-     setJoke(data)
-     setJokeExist(!jokeExist)
-     
-console.log(data)
-} catch (e) {
-     console.log(e)
-   }
- }
- 
- let showAns = ()=>{
-   setShow('show')
-   setJokeExist(!jokeExist)
- }
-  
-  return(
-    <div className='bot_container'>
- <div className='joke'>
-     <h2>{joke.setup}
-     </h2>    
-     <h3 className={show}> { joke.delivery }
-     </h3>
-     
-     </div>
-     
-     <button onClick={jokeExist ? showAns: getJoke}>{jokeExist ? 'show' : 'New joke'}
-     </button>
+  let [joke, setJoke] = useState({});
+
+  let [jokeExist, setJokeExist] = useState(false);
+
+  let [show, setShow] = useState("");
+
+  let getJoke = async () => {
+    try {
+      setShow("");
+      let apiUrl = "https://v2.jokeapi.dev/joke/Any?type=twopart";
+
+      let res = await fetch(apiUrl);
+
+      let data = await res.json();
+
+      setJoke(data);
+
+      setJokeExist(!jokeExist);
+
+      ssu.text = data.setup;
+
+      speechSynthesis.speak(ssu);
+
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  let showAns = () => {
+    setShow("show");
+    setJokeExist(!jokeExist);
+
+    ssu.text = joke.delivery;
+
+    speechSynthesis.speak(ssu);
+  };
+
+  function handleVoices(e) {
+    ssu.voice = ssg.find((voice) => {
+      return voice.name === e.target.value;
+    });
+
+    console.log(ssu.voice);
+
+    speechSynthesis.speak(ssu);
+  }
+
+  useEffect(() => {
+   
+      getJoke();
+
+    // eslint-disable-next-line
+  }, []);
+
+  console.log("'won't work on all browser")
+  return (
+    <div className="bot_container relative">
+      <div className="joke">
+        <h2 className="text-xl  font-bold">{joke.setup}</h2>
+        <h3 className={show}> {joke.delivery}</h3>
+      </div>
+
+      <button className="" onClick={jokeExist ? showAns : getJoke}>
+        {jokeExist ? "show" : "New joke"}
+      </button>
+      <select
+        className="block m-auto absolute left-0  top-0 right-0"
+        onChange={handleVoices}
+      >
+        {ssg.map((voice, i) => {
+          return (
+            <option key={i} value={voice.name}>
+              {voice.name}
+            </option>
+          );
+        })}
+      </select>
     </div>
-    )
-}
-export default App
+  );
+};
+export default App;
